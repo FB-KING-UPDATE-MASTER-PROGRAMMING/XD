c3b2a73f703be9aa4dee33223db9e75208daed25
FB-KING-UPDATE-MASTER-PROGRAMMING <115982417+FB-KING-UPDATE-MASTER-PROGRAMMING@users.noreply.github.com>
2022-12-01 22:46:33 +0600

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -3,14 +3,14 @@ os.system('clear')
 os.system('figlet FB-KING |lolcat')
 print(47*'-')
 print('')
-print(f' [01] FILE   CLONING [m1]')
-print(f' [02] FILE   CLONING [m2]')
+print(f' [01] FILE  CLONING [M1]')
+print(f' [02] FILE  CLONING [M2]')
 print(47*'-')
 A=input(f' >>> Choose Menu :')
 if A in ["1","01"]:
-    import FILE1
+    import FILE-BD
 elif A in ["2","02"]:
-    import FILE2
+    import FILE-OK
 else:
     print('Dhur Halar Put 1&2 Input Kor😾')
     time.sleep(7)
