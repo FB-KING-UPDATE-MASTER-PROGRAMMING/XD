d071100ad5280734fbfb55a4b65a91c193e5e8e2
FB-KING-UPDATE-MASTER-PROGRAMMING <115982417+FB-KING-UPDATE-MASTER-PROGRAMMING@users.noreply.github.com>
2022-12-01 16:17:21 +0600

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -6,7 +6,7 @@ print('')
 print(f' [01] FILE   CLONING [m1]')
 print(f' [02] FILE   CLONING [m2]')
 print(47*'-')
-A=input(f' >>> Choose Menu :|lolcat')
+A=input(f' >>> Choose Menu :')
 if A in ["1","01"]:
     import FILE1
 elif A in ["2","02"]:
