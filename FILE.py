ab4d3d77cd8e222713758dd2f772993b8db9bca2
FB-KING-UPDATE-MASTER-PROGRAMMING <115982417+FB-KING-UPDATE-MASTER-PROGRAMMING@users.noreply.github.com>
2022-12-01 15:52:58 +0600

    Create FILE.py

---
+++ FILE.py
@@ -0,0 +1,13 @@
+import os
+os.system('clear')
+os.system('figlet FB-KING lolcat')
+print(47*'-')
+print('')
+print(f' [01] FILE   CLONING [MAX]')
+print(f' [02] RANDOM CLONING [BEST]')
+print(47*'-')
+A=input(f' >>> Choose Menu :')
+if A in ["1","01"]:
+    import FILE1
+elif A in ["2","02"]:
+    import FILE2
