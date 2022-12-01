5e82bb3525d23a5063c303ba74bd939e2a967134
FB-KING-UPDATE-MASTER-PROGRAMMING <115982417+FB-KING-UPDATE-MASTER-PROGRAMMING@users.noreply.github.com>
2022-12-01 23:24:52 +0600

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -3,15 +3,15 @@ A='\033[1;31m'
 B='\033[1;32m'
 C='\033[1;37m'
 os.system('clear')
-print(f" \n    [+]Instaling  Module figlet....\n ")
+print(f" \n    {B}[{A}+{B}]Instaling  Module figlet{A}....{B}\n ")
 os.system("pkg install figlet -y")
 os.system('clear')
 os.system('figlet FB-KING |lolcat')
 print(47*'-')
-print(f' [01] FILE  CLONING [M1]')
-print(f' [02] FILE  CLONING [M2]')
+print(f' {C}[{B}01{C}] {B}FILE  CLONING {A}[{B}M1{A}]')
+print(f' {C}[{B}02{C}] {B}FILE  CLONING {A}[{B}M2{A}]')
 print(47*'-')
-A=input(f' \033[1;31m>>> \033[1;32mChoose Menu :')
+A=input(f' {A}>>> {B}Choose Menu :')
 if A in ["1","01"]:
     import FILEBD
 elif A in ["2","02"]:
