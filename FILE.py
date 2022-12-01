aab99459ef5cf85308ff845bfb23687e1ccc45ea
FB-KING-UPDATE-MASTER-PROGRAMMING <115982417+FB-KING-UPDATE-MASTER-PROGRAMMING@users.noreply.github.com>
2022-12-01 15:59:37 +0600

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -4,7 +4,7 @@ os.system('figlet FB-KING |lolcat')
 print(47*'-')
 print('')
 print(f' [01] FILE   CLONING [m1]')
-print(f' [02] FILE CLONING [m2]')
+print(f' [02] FILE   CLONING [m2]')
 print(47*'-')
 A=input(f' >>> Choose Menu :')
 if A in ["1","01"]:
