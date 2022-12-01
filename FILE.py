ac3500af26a0c9aff1ce00233f44bf38759d2a1e
FB-KING-UPDATE-MASTER-PROGRAMMING <115982417+FB-KING-UPDATE-MASTER-PROGRAMMING@users.noreply.github.com>
2022-12-01 16:10:29 +0600

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -1,4 +1,4 @@
-import os
+import os,time
 os.system('clear')
 os.system('figlet FB-KING |lolcat')
 print(47*'-')
@@ -11,3 +11,7 @@ if A in ["1","01"]:
     import FILE1
 elif A in ["2","02"]:
     import FILE2
+else:
+    print('Dhur Halar Put 1&2 Input Kor😾')
+    time.slep(7)
+    exit()
